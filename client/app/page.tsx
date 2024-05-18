43142a28e565598e1d8a7e89653720835ceaeb0a
PranavGopinath <125942307+PranavGopinath@users.noreply.github.com>
2024-05-18 17:20:25 -0400

    commit

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,8 +3,18 @@ import React from 'react'
 const Landing = () => {
   return (
     <div>
-      Landing
+      <div className = "flex flex-col justify-center items-center h-200 grow">
+        <h1 className = "text-white flex justify-center text-6xl pt-10">
+        Estate Eagle
+        </h1>
+        <p className = "text-white flex justify-center text-2xl pt-20">Expert Insights for Informed Home Buying</p>
       </div>
+      <div>
+        <h1 className = "text-white flex items-start text-3xl">
+
+        </h1>
+      </div>
+    </div>
   )
 }
 export default Landing
