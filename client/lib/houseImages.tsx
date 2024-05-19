97020cf8436f280961763d6d12e98eda4ad332ed
PranavGopinath <125942307+PranavGopinath@users.noreply.github.com>
2024-05-19 00:33:06 -0400

    Removed lib from gitignore

---
+++ client/lib/houseImages.tsx
@@ -0,0 +1,23 @@
+
+const images =[
+    '/houses/house1.jpg',
+    '/houses/house2.jpg',
+    '/houses/house3.jpg',
+    '/houses/house4.jpg',
+    '/houses/house5.jpg',
+    '/houses/house6.jpg',
+    '/houses/house7.jpg',
+    '/houses/house8.jpg',
+    '/houses/house9.jpg',
+    '/houses/house10.jpg',
+    '/houses/house11.jpg',
+    '/houses/house12.jpg',
+    '/houses/house13.jpg',
+    '/houses/house14.jpg',
+    '/houses/house15.jpg'
+];
+
+// Output the array to the console (for verification)
+console.log(images);
+
+export default images;
