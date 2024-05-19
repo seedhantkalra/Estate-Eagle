a9bc05fc4522e9d1eec3ea91ce12c77263829193
PranavGopinath <125942307+PranavGopinath@users.noreply.github.com>
2024-05-18 22:18:23 -0400

    Added array of images, mapping over them to display them on dashboard, with images changing locations upon reload

--- client/app/(root)/(home)/dashboard/listingCard.tsx
+++ client/app/(root)/(home)/dashboard/listingCard.tsx
@@ -1,34 +1,34 @@
 "use client";
-import { useState } from "react";
+import Image from 'next/image';
 
 interface Address {
     streetNumber: string;
     streetName: string;
     streetSuffix: string;
-  }
-  
+}
+
 interface Details {
     numBedrooms: string;
     numBathrooms: string;
-  }
-  
+}
+
 interface Listing {
     mlsNumber: string;
     listPrice: string;
     address: Address;
     details: Details;
-  }
+}
 
-const ListingCard = ( { listing } : { listing: Listing } ) => {
-
-    return ( 
+const ListingCard = ({ listing, imageSrc }: { listing: Listing, imageSrc: string }) => {
+    return (
         <div className="card rounded-md p-4 text-left h-96 bg-smokeGrey">
+            <Image src={imageSrc} alt={`Image of ${listing.address.streetNumber} ${listing.address.streetName}`} width={400} height={300} className="w-full h-48 object-cover rounded-md"/>
             <h2 className="text-center text-xl m-4"><b>{listing.address.streetNumber} {listing.address.streetName} {listing.address.streetSuffix}</b></h2>
             <p><b className="text-main">Price:</b> ${listing.listPrice}</p>
-            <p><b className="text-main">Bedrooms:</b>{listing.details.numBedrooms}</p>
+            <p><b className="text-main">Bedrooms:</b> {listing.details.numBedrooms}</p>
             <p><b className="text-main">Bathrooms:</b> {listing.details.numBathrooms}</p>
         </div>
-     );
+    );
 }
- 
+
 export default ListingCard;
